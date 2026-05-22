220259d996edb86c186204218c6b22eeb6299c35
Aditya Argadinata <93075183+AdityaArgadinata@users.noreply.github.com>
2026-05-22 23:41:35 +0700

    Remove Ngrok configuration from Next.js config and update package-lock.json

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // Tambahkan baris ini untuk mengizinkan Ngrok
-  allowedDevOrigins: [
-    "3181-2404-8000-1023-1816-2929-4844-38e6-518a.ngrok-free.app"
-  ],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
